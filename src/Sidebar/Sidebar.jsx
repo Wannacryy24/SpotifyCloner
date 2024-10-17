28d54cb8cb210b8a6f0c149aa5915506ac17ff2c
Mayank Verma <mayankverma.dev@gmail.com>
2024-10-17 15:04:43 +0530

    worked on css

--- src/Sidebar/Sidebar.jsx
+++ src/Sidebar/Sidebar.jsx
@@ -44,7 +44,6 @@ export default function Sidebar(){
               />
             </div>
           </div>
-    
           <div className="content">
                 <Testing/>
                 <NewReleases/>
