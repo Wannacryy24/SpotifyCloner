23af9f291ec61d06e2c2a66b677ff42fbf58ddb4
Mayank Verma <mayankverma.dev@gmail.com>
2024-10-18 16:12:39 +0530

    committed in newBranch at 4 PM

--- src/App.jsx
+++ src/App.jsx
@@ -38,3 +38,13 @@ export default function App(){
     </>
   )
 }
+
+//Clientid = 10365053c8d0421e89557a459da18b5e
+//secretid = af68b99be77f4206932332b7f6b24929
+
+
+// curl "https://api.spotify.com/v1/artists/4Z8W4fKeB5YxbusRsdQVPb" \
+// -H "Authorization: Bearer  BQDBKJ5eo5jxbtpWjVOj7ryS84khybFpP_lTqzV7uV-T_m0cTfwvdn5BnBSKPxKgEb11"
+
+
+//Day 6 git commands
